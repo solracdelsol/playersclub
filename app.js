0d58b7febb3ca9ce281e43dc598fa8345ced805d
espinalk212 <espinalk212@gmail.com>
2020-08-10 10:27:53 -0400

    start user auth

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongooose 
+  .connect(db, { useNewUrlParser: true})
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Server is running on port ${port}'))
