ee7fee047d531c9060115b8bcb64aac77e0e6ff5
keychained <kihoonchoi91@gmail.com>
2020-08-11 11:23:47 -0400

    Homepage rework

--- frontend/src/components/home/home_page.js
+++ frontend/src/components/home/home_page.js
@@ -7,12 +7,18 @@ class HomePage extends React.Component {
   render() {
     return (
       <>
-      <div className="homepage-background">
-        <div className="logo">
-          <h1 className="logo-label">PlayersClub</h1>
+        <div className="homepage-background">
+          <div className="logo">
+            <h1 className="logo-label">PlayersClub</h1>
+          </div>
+          <div className="nav-bar">
+            <h2 className="nav-btn">Home</h2>
+            <h3 className="nav-btn">Sports</h3>
+            <h4 className="nav-btn">Teams</h4>
+            <h5 className="nav-btn">Sign In</h5>
+          </div>
+          <footer className="footer">Copyright 2020</footer>
         </div>
-        <footer className="footer">Copyright 2020</footer>
-      </div>
       </>
     );
   }
