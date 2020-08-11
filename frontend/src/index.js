eafdd3b03b8460652ddd929398e96782d8152656
keychained <kihoonchoi91@gmail.com>
2020-08-10 21:44:08 -0400

    Homepage styling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import { logout } from './actions/session_actions';
+import axios from 'axios';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -30,3 +31,5 @@ document.addEventListener('DOMContentLoaded', () => {
   
   ReactDOM.render(<Root store={store} />, root);
 });
+
+window.axios = axios;
