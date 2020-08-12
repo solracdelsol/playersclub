5dba84896910b693e4379f5ed561a60e11437492
keychained <kihoonchoi91@gmail.com>
2020-08-12 16:46:58 -0400

    Modal css rework

--- frontend/src/components/players_bar/players_bar.js
+++ frontend/src/components/players_bar/players_bar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./players_bar.css";
 import "../../reset.css";
+import { Link } from 'react-router-dom';
 
 class PlayersBar extends React.Component {
   constructor(props) {
@@ -16,7 +17,7 @@ class PlayersBar extends React.Component {
           <h1 className="logo-label">PlayersClub</h1>
         </div>
         <div className="nav-bar">
-          <h2 className="nav-btn">Home</h2>
+          <Link className='nav-btn' to='/'>Home</Link>
           <h3 className="nav-btn">Sports</h3>
           <h4 className="nav-btn">Teams</h4>
           <input type="text" className="search" placeholder="Search"></input>
@@ -25,7 +26,7 @@ class PlayersBar extends React.Component {
             className="nav-signup">
               Join the Club!
           </button>
-          <i id="gear" className="fa fa-gear"></i>
+          <button className="gear"><i className="fa fa-gear fa-lg"></i></button>
           <button className='nav-logout' onClick={() => this.props.logout()}>Exit Club</button>
         </div>
       </>
