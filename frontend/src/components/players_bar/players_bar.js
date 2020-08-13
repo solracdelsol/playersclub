c5ccd58b1ec00f7658a46b6deeb5a22be0200b3e
keychained <kihoonchoi91@gmail.com>
2020-08-13 17:06:56 -0400

    Search splash rework

--- frontend/src/components/players_bar/players_bar.js
+++ frontend/src/components/players_bar/players_bar.js
@@ -30,7 +30,7 @@ class PlayersBar extends React.Component {
             className="nav-signup">
               Join the Club!
           </button>
-          <button className="gear"><i className="fa fa-gear fa-lg"></i></button>
+          <button className="gear"><i className="fa fa-gear fa-2x"></i></button>
           <button className='nav-logout' onClick={() => this.props.logout()}>Exit Club</button>
         </div>
       </>
