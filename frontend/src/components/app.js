821da3abfd3936d831f280ba7b494c7106ead6fd
espinalk212 <espinalk212@gmail.com>
2020-08-13 18:57:51 -0400

    render scores

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch , Route} from 'react-router-dom';
 import HomePageContainer from './home/home_page_container';
-import Modal from './modal/modal.jsx'
+import Modal from './modal/modal.jsx';
+import SearchContainer from './search/search_container';
 
 const App = () => (
   <div>
     <Modal/>
+    {/* // will render the first component to match */}
     <Switch>
-        <Route path='/' component={HomePageContainer} /> 
+        <Route path='/search' component={SearchContainer} />
+        <Route path='/' component={HomePageContainer} />
     </Switch>
   </div>
 );
--- frontend/src/components/home/home_page.js
+++ frontend/src/components/home/home_page.js
@@ -8,35 +8,45 @@ import Score from '../score/score';
 
 
 
+
 class HomePage extends React.Component {
   constructor(props){
     super(props);
+    this.getTodaysDate = this.getTodaysDate.bind(this);
   }
 
   componentDidMount(){
-    const trial = "mlb/trial/v6.6";
-    // const date = 2020/08/12
-    const event_id = "364c14ac-4862-42f0-a86c-4c78a366421d";
-    const key = "tsnfm7psc9gpakapew4v3wby";
+    if (Object.entries(this.props.sports).length === 0) {
+      this.props.mlbScheduleObj(this.getTodaysDate())
+     }
+  }
 
-    this.props.fetchAll(trial, event_id, key);
+  getTodaysDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0");
+    var yyyy = today.getFullYear();
+    return today = yyyy + "/" + mm + "/" + dd;
   }
   
   render() {
-    debugger;
     return (
       <>
         <div className="homepage-container">
           <div className="homepage-background">
-            <PlayersBar openModal={this.props.openModal} logout={this.props.logout}/>
-          <div className="information-container">
-            {
-              Object.entries(this.props.sports).length !== 0 ? 
-              // <Article />,
-              (<Score data={this.props.sports.data}  />) : null
-       
-            }
-          </div>
+            <PlayersBar
+              openModal={this.props.openModal}
+              logout={this.props.logout}
+            />
+            <div className="information-container">
+              {Object.entries(this.props.sports).length !== 0 ? (
+                <Score
+                  fetchGameScore={this.props.fetchGameScore}
+                  sports={this.props.sports}
+                />
+              ) : null}
+              <Article />
+            </div>
             <Footer />
           </div>
         </div>
