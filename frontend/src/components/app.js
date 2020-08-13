740f3d68d040fe5bfb555f8331b365893463b1c4
mfung <mjfung1@gmail.com>
2020-08-13 18:08:08 -0400

    Articles

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,7 +4,7 @@ import { Switch , Route} from 'react-router-dom';
 import HomePageContainer from './home/home_page_container';
 import Modal from './modal/modal.jsx';
 import SearchContainer from './search/search_container';
-import Modal from './modal/modal.jsx'
+// import Modal from './modal/modal.jsx'
 import PreferencesContainer from '../components/preferences/preferences_container'
 
 
