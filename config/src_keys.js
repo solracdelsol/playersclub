c46d4a3cc84541a539de9ef2971e60f163df7738
keychained <kihoonchoi91@gmail.com>
2020-09-04 17:13:26 -0400

    updated

--- config/src_keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MLBkey: 'tsnfm7psc9gpakapew4v3wby',
-    NBAkey: 'xd7br7gx5wscbvyg8ftuh6ty',
-    NHLkey: 'b44qqmht334vhcg5g8uhpygy',
-    MLBTrial: 'mlb/ trial / v7',
-NBATrial: 'nba / trial / v7',
-NHLTrial: 'nhl / trial / v7',
-}
--- frontend/src/components/score/score.js
+++ frontend/src/components/score/score.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './score.css';
 import '../../reset.css';
-import { MLBkey, MLBTrial, NBATrial, NBAkey, NHLTrial, NHLkey  } from '../../config/src_keys';
+import { MLBkey, MLBTrial, NBATrial, NBAkey, NHLTrial, NHLkey  } from "../../config/src_keys"
 
 
 class Score extends React.Component {
