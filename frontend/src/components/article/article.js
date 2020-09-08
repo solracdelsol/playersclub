8a550f12643258764cfb25d6af67b8f2282f7f47
espinalk212 <espinalk212@gmail.com>
2020-09-08 10:43:10 -0400

    implement article feature

--- frontend/src/components/article/article.js
+++ frontend/src/components/article/article.js
@@ -3,30 +3,35 @@ import './article.css';
 import '../../reset.css';
 
 class Article extends React.Component {
-    render() {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.props.fetchArticles();
+  }
+  render() {
+    if (Object.entries(this.props.articles).length === 0) 
+    { return null 
+    } else {
+      const articleCard = this.props.articles.data.articles.map((article,idx) => {
         return (
-          <div className="article-container">
-            <div className="articles">
-              <p className="article-header">Latest Article</p>
-              {/* <object
-                className="article"
-                type="text/html"
-                data="https://theathletic.com/mlb/#game-59835">
-              </object> */}
-              {/* <object
-                className="article"
-                type="text/html"
-                data="https://theathletic.com/1992551/2020/08/13/intel-sharing-what-our-insiders-hear-about-the-pelicans-wizards-and-kings/#holiday-banner">
-              </object>
-              <object
-                className="article"
-                type="text/html"
-                data="https://theathletic.com/1993180/2020/08/13/its-surreal-why-trevor-rosenthals-success-with-the-royals-is-so-profound/#holiday-banner">
-              </object> */}
-            </div>
-          </div>
+          <>
+          <a href={article.url} >{article.title}</a>
+          </>
         );
+      })
+    return (
+      
+      <div className="article-container">
+        <div className="articles">
+          <p className="article-header">Latest Article</p>
+          {articleCard}
+        </div>
+      </div>
+    );
     }
+  }
 }
 
 export default Article;
---
+++ frontend/src/reducers/articles_reducer.js
@@ -0,0 +1,18 @@
+import { 
+  RECEIVE_ALL_ARTICLES,
+  CLEAR_ALL_ARTICLES
+} from '../actions/article_actions';
+
+const ArticlesReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch(action.type) {
+    case RECEIVE_ALL_ARTICLES:
+      return action.articles
+    case CLEAR_ALL_ARTICLES:
+      return {}
+    default: 
+      return oldState;
+  }
+}
+
+export default ArticlesReducer;
